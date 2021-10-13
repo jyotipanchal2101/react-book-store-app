2dc8e144a234184245cbdd1c68bd42733c224846
Jyoti Panchal <jyoti.panchal@cuelogic.com>
2021-10-13 13:26:54 +0530

    added hooks

--- src/containers/UserList/UserList.js
+++ src/containers/UserList/UserList.js
@@ -1,40 +1,40 @@
-import React, { Component } from "react";
+import React, {useEffect } from 'react';
+
 import { getUserList } from "../../redux/actions/userAction";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { Table } from "semantic-ui-react";
 
-export class MyOrders extends Component {
-  componentDidMount() {
-    this.props.getUserList();
-  }
-  render() {
-    const { userlist } = this.props;
-    return (
-      <div>
-        <Table celled padded>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell singleLine>First Name</Table.HeaderCell>
-              <Table.HeaderCell>last Name</Table.HeaderCell>
-              <Table.HeaderCell>User</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
+export const MyOrders = (props) => {
+  const { userlist } = this.props;
+useEffect(() => {
+  this.props.getUserList();
+}, [])
+  return (
+    <div>
+      <Table celled padded>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell singleLine>First Name</Table.HeaderCell>
+            <Table.HeaderCell>last Name</Table.HeaderCell>
+            <Table.HeaderCell>User</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-          <Table.Body>
-            {userlist.map((userlist) => (
-              <Table.Row>
-                <Table.Cell>{userlist.firstname}</Table.Cell>
-                <Table.Cell>{userlist.lastname}</Table.Cell>
-                <Table.Cell>{userlist.usertype}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    );
-  }
-}
+        <Table.Body>
+          {userlist.map((userlist) => (
+            <Table.Row>
+              <Table.Cell>{userlist.firstname}</Table.Cell>
+              <Table.Cell>{userlist.lastname}</Table.Cell>
+              <Table.Cell>{userlist.usertype}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </div>
+  );
+};
+
 
 const mapStateToProps = (state) => {
   return {
